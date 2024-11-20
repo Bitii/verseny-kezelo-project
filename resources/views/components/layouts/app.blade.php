a419acce3e7170bc5897d7ca2625e6af9c164b7c
Tibi <tari.tibor70@gmail.com>
2024-11-20 11:29:21 +0100

    bootstrap bugfix

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    @vite('resources/js/app.js')
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>{{ env('APP_NAME') }}</title>
 </head>
 
